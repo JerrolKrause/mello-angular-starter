487ad10b06715927db9049532c71b7f33791eb1c
LD\jkrause <jkrause@loandepot.com>
2017-04-13 13:58:09 -0700

    Added bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,7 +30,6 @@ import { NoContentComponent } from './no-content';
 import { XLargeDirective } from './home/x-large';
 
 import '../styles/styles.scss';
-import '../styles/headings.css';
 
 // Application wide providers
 const APP_PROVIDERS = [
