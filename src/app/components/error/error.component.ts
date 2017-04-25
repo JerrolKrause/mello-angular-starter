dc7180e816b3802959ed0369adc36e252afd151d
LD\jkrause <jkrause@loandepot.com>
2017-04-24 21:45:15 -0700

    Added additional styling

---
+++ src/app/components/error/error.component.ts
@@ -0,0 +1,25 @@
+﻿import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+    selector: 'error',
+    templateUrl: './error.component.html'
+})
+export class ErrorComponent implements OnInit {
+
+    public errorOfKeys: string[]; // Array of keys in API error response
+    @Input() error: IErrorApi; // The error object 
+    private ignoreProps: string[] = ['_body', 'headers', 'errorMsg']; // API response keys to ignore
+
+    constructor(
+    ) { }
+
+    ngOnInit() {
+        // Create an array of keys to loop through and filter out anything on the ignore list
+        this.errorOfKeys = Object.keys(this.error).filter((key) => {
+            if (this.ignoreProps.indexOf(key) == -1) {
+                return key;
+            }
+        });
+    }
+
+}
